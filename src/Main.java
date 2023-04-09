0beef801f9a772fd89cf5a2a3016ebbcdb4a924b
Smile-369 <105698549+Smile-369@users.noreply.github.com>
2023-04-09 17:57:18 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
+import java.sql.*;
+
+public class DatabaseConnection {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Connect to the database
+            String url = "jdbc:mysql://localhost:3306/mydatabase";
+            String username = "root";
+            String password = "mypassword";
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Execute a query
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM mytable");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("column1") + " " + resultSet.getString("column2"));
+            }
+
+            // Close the connection
+            connection.close();
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
     }
 }
