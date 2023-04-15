64c4171e6316cba6a89e3eec7d4cd3b148275462
Smile-369 <105698549+Smile-369@users.noreply.github.com>
2023-04-16 00:28:21 +0800

    Only deletes when status is not D and there is no enclosing asset

--- src/Javacode/Javacoe/DatabaseConnection.java
+++ src/Javacode/Javacoe/DatabaseConnection.java
@@ -41,6 +41,30 @@ public class DatabaseConnection {
             System.err.println("SQLException: " + e.getMessage());
         }
     }
+    public void insertToAsset(int assetId, String assetName, String assetDescription, Date acquisitionDate,
+                              boolean forRent, double assetValue, String typeAsset, String status,
+                              double locLatitude, double locLongitude, String hoaName) {
+        try {
+            String query = "INSERT INTO assets (asset_id, asset_name, asset_description, acquisition_date, forrent," +
+                    " asset_value, type_asset, status, loc_lattitude, loc_longiture, hoa_name) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1, assetId);
+            pstmt.setString(2, assetName);
+            pstmt.setString(3, assetDescription);
+            pstmt.setDate(4, new java.sql.Date(acquisitionDate.getTime()));
+            pstmt.setBoolean(5, forRent);
+            pstmt.setDouble(6, assetValue);
+            pstmt.setString(7, typeAsset);
+            pstmt.setString(8, status);
+            pstmt.setDouble(9, locLatitude);
+            pstmt.setDouble(10, locLongitude);
+            pstmt.setString(11, hoaName);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        }
+    }
 
     public ArrayList<String> displayAssets() {
         ArrayList<String> assetList = new ArrayList<>();
@@ -64,27 +88,101 @@ public class DatabaseConnection {
 
     public void deleteAsset(int assetID) {
         try {
-            String query = "DELETE FROM assets WHERE asset_id=" + assetID;
+            String query = "DELETE FROM assets WHERE status !='D' AND enclosing_asset IS null AND asset_id=" + assetID;
             statement.executeUpdate(query);
         } catch (SQLException e) {
             System.err.println("SQLException: " + e.getMessage());
         }
     }
+    public void updateAsset(int assetID){
+        String query ="";
+    }
 
-    public void disposeAsset(String assetID) {
+    public void disposeAsset(int assetID) {
         try {
-            String query = "UPDATE assets SET status='D' WHERE asset_id=" + assetID;
+            String query = "UPDATE assets SET status='D' WHERE enclosing_asset= "+assetID+ " OR asset_id=" + assetID;
             statement.executeUpdate(query);
         } catch (SQLException e) {
             System.err.println("SQLException: " + e.getMessage());
         }
     }
 
+    public void recordRental(int assetId, Date rentDate, Date reserveDate, int residentId, double rentAmt,
+                             double discount, String status, String inspectDetails, double assessedVal,
+                             int acceptHOId, String acceptPos, Date acceptElecDate, Date returnDate) {
+        try {
+            String query = "INSERT INTO asset_rentals (asset_id, rental_date, reservation_date, resident_id," +
+                    " rental_amount, discount, status, inspection_details, assessed_value, accept_hoid," +
+                    " accept_position, accept_electiondate, return_date) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1, assetId);
+            pstmt.setDate(2, new java.sql.Date(rentDate.getTime()));
+            pstmt.setDate(3, new java.sql.Date(reserveDate.getTime()));
+            pstmt.setInt(4, residentId);
+            pstmt.setDouble(5, rentAmt);
+            pstmt.setDouble(6, discount);
+            pstmt.setString(7, status);
+            pstmt.setString(8, inspectDetails);
+            pstmt.setDouble(9, assessedVal);
+            pstmt.setInt(10, acceptHOId);
+            pstmt.setString(11, acceptPos);
+            pstmt.setDate(12, new java.sql.Date(acceptElecDate.getTime()));
+            pstmt.setDate(13, new java.sql.Date(returnDate.getTime()));
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        }
+    }
+
+    public void returnRental(String assetId) {
+        try {
+            String query = "UPDATE asset_rentals " +
+                    "SET status='N' WHERE asset_id=" + assetId;
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        }
+    }
+
+    public void updateRental(String assetId, Date rentDate, Date reserveDate, int residentId, double rentAmt,
+                             double discount, String status, String inspectDetails, double assessedVal,
+                             int acceptHOId, String acceptPos, Date acceptElecDate, Date returnDate) {
+        try {
+            String query = "UPDATE asset_rentals " +
+                    "SET rental_date=" + rentDate + ", " +
+                    "reservation_date=" + reserveDate + ", " +
+                    "resident_id=" + residentId + ", " +
+                    "rental_amount=" + rentAmt + ", " +
+                    "discount=" + discount + ", " +
+                    "status='" + status + "', " +
+                    "inspection_details='" + inspectDetails + "', " +
+                    "assessed_value=" + assessedVal + ", " +
+                    "accept_hoid=" + acceptHOId + ", " +
+                    "accept_position='" + acceptPos + "', " +
+                    "acccept_electiondate=" + acceptElecDate + ", " +
+                    "return_date=" + returnDate + " " +
+                    "WHERE asset_id=" + assetId;
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        }
+    }
+
+    public void deleteRental(String assetId) {
+        try {
+            String query = "UPDATE asset_rentals " +
+                    "SET status='N' WHERE asset_id=" + assetId;
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        }
+    }
     public static void main(String[] args) {
         DatabaseConnection databaseConnection= new DatabaseConnection();
         databaseConnection.insertToAsset(1222, "chair", "String assetDescription", new Date(2023,4,15),
                 true, 312.21,"P", "W",
-                312.21, 312.21, "SJH",null);
+                312.21, 312.21, "SJH");
         ArrayList<String> test=databaseConnection.displayAssets();
         for (String s : test) {
             System.out.println(s);
